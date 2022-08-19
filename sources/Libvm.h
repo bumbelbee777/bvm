948f013d32a091d8d226d8da58889b0c3fa77b48
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2022-08-19 12:04:55 +0100

    fixes?

--- sources/Libvm.h
+++ sources/Libvm.h
@@ -1,10 +1,20 @@
 #pragma once
 
-#include <CPU.hpp>
-#include <Flags.hpp>
-#include <RAM.hpp>
+namespace libvm {
+    namespace CPU {
+        void SetDreg1Data(uint8_t Value);
+        void SetDreg2Data(uint8_t Value);
+        void GetDreg1Data(void);
+        void GetDreg2Data(void);
+        void StartCPU(uint8_t RamSize);
+        void HaltCPU(void);
+    }
 
-using namespace libvm;
-using namespace libvm::CPU;
-using namespace libvm::RAM;
-using namespace libvm::Flags;
+    namespace Flags {
+        void DebugPrint(string Message);
+    }
+
+    namespace RAM {
+       uint8_t ReadIndex(uint8_t Index); 
+    }
+}
---
+++ sources/RAM.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Libvm.h>
+#include <iostream>
+
+using namespace libvm::Flags;
+using namespace std;
+
+namespace libvm {
+    namespace RAM {
+        uint8_t ReadIndex(uint8_t Index) {
+            if(Index = NULL /*|| Index > RAMSize*/) {DebugPrint("error while reading index, exiting..."); return 1;}
+        }
+    }
+}
