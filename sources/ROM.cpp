3c339f1218e670f83ab344e1b71b0df5eb6671a6
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2022-08-26 10:23:57 +0100

    changes

---
+++ sources/ROM.cpp
@@ -0,0 +1,27 @@
+#include <Libvm.h>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace libvm;
+
+namespace libvm {
+    namespace ROM {
+        vector<uint8_t> Read(const char* FileName) {
+            ifstream file(FileName, ios::binary);
+            if(file.good()) {
+                streampos FileSize;
+                file.seekg(0, ios::end);
+                FileSize = file.tellg();
+                file.seekg(0, ios::beg);
+                vector<uint8_t> FileData(FileSize);
+                file.read((char*) &FileData[0], FileSize);
+                return FileData;
+            } else {
+                DebugPrint("couldn't read ROM file!");
+                return;
+            }
+        }
+    }
+}
